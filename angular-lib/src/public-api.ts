2a1abb082f58987880b89d48e1e29204dff76a18
Josh Weisman <josh.m.weisman@gmail.com>
2020-03-10 06:15:03 +0200

    Expose toFormGroup

--- angular-lib/src/public-api.ts
+++ angular-lib/src/public-api.ts
@@ -3,3 +3,4 @@ export * from './angular/services';
 export * from './angular/modules';
 export * from './lib/public-interfaces';
 export { Request, HttpMethod } from './angular/services/cloudapp-rest.service';
+export { toFormGroup } from './angular/services/form-group-util';
