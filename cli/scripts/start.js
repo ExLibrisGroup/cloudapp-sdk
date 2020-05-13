f8e79b70ed3b8ce73b2ccce89f79543025004eee
Josh Weisman <josh.m.weisman@gmail.com>
2020-05-13 12:10:06 +0300

    Revert "Copy .vscode directory on start"
    This reverts commit 1793893c94365f6f1a9b62e63b623fc88e011a13.

--- cli/scripts/start.js
+++ cli/scripts/start.js
@@ -1,7 +1,5 @@
 const ora = require("ora");
 const chalk = require("chalk");
-const util = require("util");
-const ncp = util.promisify(require("ncp").ncp);
 
 const { startWatcher } = require("../lib/watch");
 const { checkConfig } = require("../lib/config/config");
@@ -9,18 +7,14 @@ const { updateManifest } = require("../lib/config/manifest");
 const { startDev } = require("../lib/action");
 const { syncNgDir } = require("../lib/work");
 const { install } = require("../lib/install");
-const { appBaseDir, cwd } = require("../lib/dirs");
 
 const doInstall = process.argv.indexOf('--no-install') === -1;
 const openBrowser = process.argv.indexOf('--no-open-browser') === -1;
-
-const copyVsDir = () => ncp(`${appBaseDir}/.vscode`, `${cwd}/.vscode`);
 
 doInstall && install();
 
 let spinner;
 syncNgDir()
-  .then(copyVsDir)
   .then(async () => {
     await checkConfig();
     updateManifest();
