1aef89d58947cc92b56ee14001a3c820000dbbd0
Josh Weisman <josh.m.weisman@gmail.com>
2021-07-20 16:51:09 +0300

    Handle Load Balancer Sticky Cookie
    URM-156660
    #48

--- base/base/.ng/src/proxy.conf.js
+++ base/base/.ng/src/proxy.conf.js
@@ -8,9 +8,16 @@ const PROXY_CONFIG = {
             let cookies =  proxyRes.headers['set-cookie'];
             cookies && cookies.forEach((val, i, arr) => {
                 const parts = val.split(";").map(p=>p.trim());
-                arr[i] = parts.filter(p=>p.match(/^(?!SameSite|Secure)/i)).join('; ');
+                arr[i] = parts.filter(p=>p.match(/^(?!SameSite|Secure)/i)).join('; ')
+                /* Handle sticky cookie for load balancer */
+                .replace(/__Secure-/g, "KUKUBALALA");
             });
             proxyRes.headers['set-cookie'] = cookies;
+        },
+        onProxyReq: (proxyReq, req, res) => {
+            let cookies = req.headers['cookie'];
+            /* Handle sticky cookie for load balancer */
+            cookies && proxyReq.setHeader('Cookie', cookies.replace(/KUKUBALALA/g, "__Secure-"));
         }
     }
 }
